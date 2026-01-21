933d8943827aebbc9aa4463fe857cf1300de2586
ShoyaKawada0825 <masaharu0825@icloud.com>
2026-01-21 16:07:05 +0900

    no message

---
+++ SearchAppForGitHubRepositories/SearchAppForGitHubRepositoriesTests/SearchAppForGitHubRepositoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppForGitHubRepositoriesTests.swift
+//  SearchAppForGitHubRepositoriesTests
+//
+//  Created by 河田翔也 on 2026/01/21.
+//
+
+import Testing
+@testable import SearchAppForGitHubRepositories
+
+struct SearchAppForGitHubRepositoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
