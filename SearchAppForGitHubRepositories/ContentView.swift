78a61b6e1b5a746710cf17a93a8bc1331b2efc6e
ShoyaKawada0825 <masaharu0825@icloud.com>
2026-01-21 15:34:24 +0900

    新規コミット

---
+++ SearchAppForGitHubRepositories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchAppForGitHubRepositories
+//
+//  Created by 河田翔也 on 2026/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchAppForGitHubRepositories/SearchAppForGitHubRepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchAppForGitHubRepositoriesApp.swift
+//  SearchAppForGitHubRepositories
+//
+//  Created by 河田翔也 on 2026/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SearchAppForGitHubRepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
